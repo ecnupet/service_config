3f445996b84c650d4dabc5181a238db22bdad7cb
shanchao <chao.shan01@liulishuo.com>
2021-03-23 17:40:42 +0800

    update

--- scripts/message.sh
+++ scripts/message.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 chmod +x ./scripts/python/message.py
-git log -1 HEAD --pretty=format:'Qiniu 已更新，提交信息：%n%n%s%n%nAuthor: %cN%n%nEmail: %ce%n%n' | ./scripts/python/message.py
+git log -1 HEAD --pretty=format:'service_config 已更新，提交信息：%n%n%s%n%nAuthor: %cN%n%nEmail: %ce%n%n' | ./scripts/python/message.py
